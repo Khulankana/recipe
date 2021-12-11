ff00b1485d66f70020e2ce6b54b8ece5fc403999
khulankana <khulan.flwr524@gmail.com>
2021-12-11 22:29:46 +0800

    state bolon search controller nemlee

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,25 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
+const state = {};
 
-search.doSearch().then((r) => console.log(r));
+const controlSearch = async () => {
+  // Webees hailtiin tulhuur ugiig gargaj avna
+  const query = "pizza";
+
+  if (query) {
+    // Shineer hailtiin objectiig uusgej ugnu
+    state.search = new Search(query);
+    // Hailt hiihed zoriulj ui-g beltgene
+    //Hailtiig guitsetgene
+    await state.search.doSearch();
+
+    //Hailtiin ur dung delgetsend uzuulne
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault(); //defualt uil ajillagaag ni boliul . submit tovch maani daragdaad l alga bolood bsan uchir uuniig zogsooj bna
+  controlSearch();
+});
